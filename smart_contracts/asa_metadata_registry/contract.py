a5c70664194da3a9e31084ff905772cde34b3154
cusma <cosimo.bassi@algorand.foundation>
2025-10-28 12:05:51 +0100

    feat: add bare create method

--- smart_contracts/asa_metadata_registry/contract.py
+++ smart_contracts/asa_metadata_registry/contract.py
@@ -1,5 +1,17 @@
-from algopy import ARC4Contract
+from algopy import Account, ARC4Contract, TemplateVar, Txn, arc4
+
+from . import errors as err
+from .template_vars import TRUSTED_DEPLOYER
 
 
 class AsaMetadataRegistry(ARC4Contract):
-    ...
+    """
+    Singleton Application providing ASA metadata via Algod API and AVM
+    """
+
+    @arc4.baremethod(create="require")
+    def create(self) -> None:
+        # Preconditions
+        assert Txn.sender == TemplateVar[Account](
+            TRUSTED_DEPLOYER
+        ), err.UNTRUSTED_DEPLOYER
