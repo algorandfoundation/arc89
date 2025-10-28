07352ec250117fe2b457345e0354fe8326d32baa
cusma <cosimo.bassi@algorand.foundation>
2025-10-28 19:16:43 +0100

    feat: define abi types

--- smart_contracts/asa_metadata_registry/abi_types.py
+++ smart_contracts/asa_metadata_registry/abi_types.py
@@ -1,4 +1,5 @@
 from typing import Literal, TypeAlias
+
 from algopy import UInt64, arc4
 
 # Type Aliases
@@ -14,13 +15,22 @@ class MbrDelta(arc4.Struct, kw_only=True):
     The variation of the ASA Metadata Registry Application Account MBR due to the
     creation, update, or deletion of the Asset Metadata Box.
     """
+
     sign: arc4.UInt8  # Enum: null (0), positive (1), or negative (255)
     amount: arc4.UInt64  # MBR amount expressed in microALGO
+
+
+class MutableFlag(arc4.Struct, kw_only=True):
+    """Mutable Metadata Identifier or Flag"""
+
+    flag: arc4.Bool
+    last_modified_round: arc4.UInt64
 
 
 # ARC-28 Events
 class Arc89MetadataCreated(arc4.Struct, kw_only=True):
     """Event emitted when Asset Metadata is created"""
+
     asset_id: arc4.UInt64
     round: arc4.UInt64
     timestamp: Timestamp
@@ -33,6 +43,7 @@ class Arc89MetadataCreated(arc4.Struct, kw_only=True):
 
 class Arc89MetadataUpdated(arc4.Struct, kw_only=True):
     """Event emitted when Asset Metadata is replaced or updated"""
+
     asset_id: arc4.UInt64
     round: arc4.UInt64
     timestamp: Timestamp
@@ -42,6 +53,7 @@ class Arc89MetadataUpdated(arc4.Struct, kw_only=True):
 
 class Arc89MetadataDeleted(arc4.Struct, kw_only=True):
     """Event emitted when Asset Metadata is deleted"""
+
     asset_id: arc4.UInt64
     round: arc4.UInt64
     timestamp: Timestamp
@@ -49,6 +61,7 @@ class Arc89MetadataDeleted(arc4.Struct, kw_only=True):
 
 class Arc89MetadataUpdatedFlags(arc4.Struct, kw_only=True):
     """Event emitted when Asset Metadata Flags are updated"""
+
     asset_id: arc4.UInt64
     round: arc4.UInt64
     timestamp: Timestamp
