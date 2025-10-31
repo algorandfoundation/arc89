91776d3b3ac155bf9a9d295d59c1775b48719eed
cusma <cosimo.bassi@algorand.foundation>
2025-10-31 10:03:03 +0100

    test: validate constants

---
+++ tests/test_constants.py
@@ -0,0 +1,7 @@
+import math
+from smart_contracts.asa_metadata_registry import constants as const
+
+def test_constants() -> None:
+    assert const.HEADER_SIZE <= const.MAX_LOG_SIZE - const.ARC4_RETURN_PREFIX_SIZE
+    assert const.MAX_PAGES == math.ceil(const.MAX_METADATA_SIZE / const.PAGE_SIZE)
+    assert const.MAX_PAGES <= 256
