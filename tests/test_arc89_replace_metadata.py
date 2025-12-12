bc358d8c4fada315cb0162ddcf65f793c5c9eb64
cusma <cosimo.bassi@algorand.foundation>
2025-12-12 11:27:55 -0300

    chore: use uploaded metadata fixtures

--- tests/test_arc89_replace_metadata.py
+++ tests/test_arc89_replace_metadata.py
@@ -11,43 +11,32 @@ from tests.helpers.utils import create_metadata, replace_metadata
 def test_replace_with_empty_metadata(
     asset_manager: SigningAccount,
     asa_metadata_registry_client: AsaMetadataRegistryClient,
-    short_metadata: AssetMetadata,
+    uploaded_short_metadata: AssetMetadata,
     empty_metadata: AssetMetadata,
 ) -> None:
-    create_metadata(
-        asset_manager=asset_manager,
-        asa_metadata_registry_client=asa_metadata_registry_client,
-        asset_id=short_metadata.asset_id,
-        metadata=short_metadata,
-    )
-    replace_mbr_delta = empty_metadata.get_mbr_delta(old_size=short_metadata.size)
+    replace_mbr_delta = empty_metadata.get_mbr_delta(old_size=uploaded_short_metadata.size)
     mbr_delta = replace_metadata(
         asset_manager=asset_manager,
         asa_metadata_registry_client=asa_metadata_registry_client,
-        asset_id=short_metadata.asset_id,
+        asset_id=uploaded_short_metadata.asset_id,
         new_metadata=empty_metadata,
     )
     assert -mbr_delta.amount == replace_mbr_delta.signed_amount
     assert mbr_delta.amount == replace_mbr_delta.amount.micro_algo
     assert mbr_delta.sign == MBR_DELTA_NEG
 
+
 def test_replace_with_smaller_metadata_size(
     asset_manager: SigningAccount,
     asa_metadata_registry_client: AsaMetadataRegistryClient,
-    maxed_metadata: AssetMetadata,
+    uploaded_maxed_metadata: AssetMetadata,
     short_metadata: AssetMetadata,
 ) -> None:
-    create_metadata(
-        asset_manager=asset_manager,
-        asa_metadata_registry_client=asa_metadata_registry_client,
-        asset_id=maxed_metadata.asset_id,
-        metadata=maxed_metadata,
-    )
-    replace_mbr_delta = short_metadata.get_mbr_delta(old_size=maxed_metadata.size)
+    replace_mbr_delta = short_metadata.get_mbr_delta(old_size=uploaded_maxed_metadata.size)
     mbr_delta = replace_metadata(
         asset_manager=asset_manager,
         asa_metadata_registry_client=asa_metadata_registry_client,
-        asset_id=maxed_metadata.asset_id,
+        asset_id=uploaded_maxed_metadata.asset_id,
         new_metadata=short_metadata,
         extra_resources=1,
     )
@@ -55,22 +44,18 @@ def test_replace_with_smaller_metadata_size(
     assert mbr_delta.amount == replace_mbr_delta.amount.micro_algo
     assert mbr_delta.sign == MBR_DELTA_NEG
 
+
 def test_replace_with_equal_metadata_size(
     asset_manager: SigningAccount,
     asa_metadata_registry_client: AsaMetadataRegistryClient,
+    uploaded_maxed_metadata: AssetMetadata,
     maxed_metadata: AssetMetadata,
 ) -> None:
-    create_metadata(
-        asset_manager=asset_manager,
-        asa_metadata_registry_client=asa_metadata_registry_client,
-        asset_id=maxed_metadata.asset_id,
-        metadata=maxed_metadata,
-    )
-    replace_mbr_delta = maxed_metadata.get_mbr_delta(old_size=maxed_metadata.size)
+    replace_mbr_delta = maxed_metadata.get_mbr_delta(old_size=uploaded_maxed_metadata.size)
     mbr_delta = replace_metadata(
         asset_manager=asset_manager,
         asa_metadata_registry_client=asa_metadata_registry_client,
-        asset_id=maxed_metadata.asset_id,
+        asset_id=uploaded_maxed_metadata.asset_id,
         new_metadata=maxed_metadata,
     )
     assert mbr_delta.amount == replace_mbr_delta.signed_amount
