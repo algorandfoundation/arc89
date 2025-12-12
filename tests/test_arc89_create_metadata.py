ba424e0277a4be4c368fab81d397a60be53b3acd
cusma <cosimo.bassi@algorand.foundation>
2025-12-12 09:05:33 -0300

    chore: use create metadata utility

--- tests/test_arc89_create_metadata.py
+++ tests/test_arc89_create_metadata.py
@@ -1,8 +1,7 @@
-from algokit_utils import SigningAccount, PaymentParams, CommonAppCallParams, \
-    SendParams, AlgoAmount
+from algokit_utils import SigningAccount
 
 from smart_contracts.artifacts.asa_metadata_registry.asa_metadata_registry_client import \
-    AsaMetadataRegistryClient, Arc89CreateMetadataArgs
+    AsaMetadataRegistryClient
 
 from tests.helpers.factories import AssetMetadata
 from tests.helpers.utils import create_metadata
@@ -14,24 +13,12 @@ def test_empty_metadata(
     empty_metadata: AssetMetadata,
 ) -> None:
     creation_mbr_delta = empty_metadata.get_mbr_delta(old_size=None)
-    mbr_payment = asa_metadata_registry_client.algorand.create_transaction.payment(
-        PaymentParams(
-            sender=asset_manager.address,
-            receiver=asa_metadata_registry_client.app_address,
-            amount=creation_mbr_delta.amount
-        )
+    mbr_delta = create_metadata(
+        asset_manager=asset_manager,
+        asa_metadata_registry_client=asa_metadata_registry_client,
+        asset_id=empty_metadata.asset_id,
+        metadata=empty_metadata,
     )
-
-    mbr_delta = asa_metadata_registry_client.send.arc89_create_metadata(
-        args=Arc89CreateMetadataArgs(
-            asset_id=empty_metadata.asset_id,
-            flags=empty_metadata.flags,
-            metadata_size=empty_metadata.size,
-            payload=empty_metadata.metadata_bytes,
-            mbr_delta_payment=mbr_payment,
-        ),
-        params=CommonAppCallParams(sender=asset_manager.address)
-    ).abi_return
     assert mbr_delta.amount == creation_mbr_delta.amount.micro_algo
     # TODO: Verify Asset Metadata Box contents matches fixture data
 
@@ -42,28 +29,12 @@ def test_short_metadata(
     short_metadata: AssetMetadata,
 ) -> None:
     creation_mbr_delta = short_metadata.get_mbr_delta(old_size=None)
-    mbr_payment = asa_metadata_registry_client.algorand.create_transaction.payment(
-        PaymentParams(
-            sender=asset_manager.address,
-            receiver=asa_metadata_registry_client.app_address,
-            amount=creation_mbr_delta.amount
-        )
+    mbr_delta = create_metadata(
+        asset_manager=asset_manager,
+        asa_metadata_registry_client=asa_metadata_registry_client,
+        asset_id=short_metadata.asset_id,
+        metadata=short_metadata,
     )
-
-    mbr_delta = asa_metadata_registry_client.send.arc89_create_metadata(
-        args=Arc89CreateMetadataArgs(
-            asset_id=short_metadata.asset_id,
-            flags=short_metadata.flags,
-            metadata_size=short_metadata.size,
-            payload=short_metadata.metadata_bytes,
-            mbr_delta_payment=mbr_payment,
-        ),
-        params=CommonAppCallParams(
-            sender=asset_manager.address,
-            max_fee=AlgoAmount(algo=1)
-        ),
-        send_params=SendParams(cover_app_call_inner_transaction_fees=True),
-    ).abi_return
     assert mbr_delta.amount == creation_mbr_delta.amount.micro_algo
     # TODO: Verify Asset Metadata Box contents matches fixture data
 
@@ -77,7 +48,10 @@ def test_maxed_metadata(
     mbr_delta = create_metadata(
         asset_manager=asset_manager,
         asa_metadata_registry_client=asa_metadata_registry_client,
+        asset_id=maxed_metadata.asset_id,
         metadata=maxed_metadata,
     )
     assert mbr_delta.amount == creation_mbr_delta.amount.micro_algo
     # TODO: Verify Asset Metadata Box contents matches fixture data
+
+# TODO: Test failing conditions
