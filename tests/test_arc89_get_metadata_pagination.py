ac3f735883713e60e149cebbbe2ba4ea283d977a
cusma <cosimo.bassi@algorand.foundation>
2025-12-12 11:28:07 -0300

    test: get metadata pagination

---
+++ tests/test_arc89_get_metadata_pagination.py
@@ -0,0 +1,48 @@
+from algokit_utils import SigningAccount
+
+from smart_contracts.artifacts.asa_metadata_registry.asa_metadata_registry_client import \
+    AsaMetadataRegistryClient, Arc89GetMetadataPaginationArgs
+from smart_contracts.asa_metadata_registry import constants as const
+
+from tests.helpers.factories import AssetMetadata
+
+
+def test_arc89_empty_metadata_pagination(
+    untrusted_account: SigningAccount,
+    asa_metadata_registry_client: AsaMetadataRegistryClient,
+    uploaded_empty_metadata: AssetMetadata,
+) -> None:
+    pagination = asa_metadata_registry_client.send.arc89_get_metadata_pagination(
+        args=Arc89GetMetadataPaginationArgs(asset_id=uploaded_empty_metadata.asset_id),
+    ).abi_return
+    assert pagination.metadata_size == uploaded_empty_metadata.size
+    assert pagination.page_size == const.PAGE_SIZE
+    assert pagination.total_pages == uploaded_empty_metadata.total_pages
+
+
+def test_arc89_short_metadata_pagination(
+    untrusted_account: SigningAccount,
+    asa_metadata_registry_client: AsaMetadataRegistryClient,
+    uploaded_short_metadata: AssetMetadata,
+) -> None:
+    pagination = asa_metadata_registry_client.send.arc89_get_metadata_pagination(
+        args=Arc89GetMetadataPaginationArgs(asset_id=uploaded_short_metadata.asset_id),
+    ).abi_return
+    assert pagination.metadata_size == uploaded_short_metadata.size
+    assert pagination.page_size == const.PAGE_SIZE
+    assert pagination.total_pages == uploaded_short_metadata.total_pages
+
+
+def test_arc89_maxed_metadata_pagination(
+    untrusted_account: SigningAccount,
+    asa_metadata_registry_client: AsaMetadataRegistryClient,
+    uploaded_maxed_metadata: AssetMetadata,
+) -> None:
+    pagination = asa_metadata_registry_client.send.arc89_get_metadata_pagination(
+        args=Arc89GetMetadataPaginationArgs(asset_id=uploaded_maxed_metadata.asset_id),
+    ).abi_return
+    assert pagination.metadata_size == uploaded_maxed_metadata.size
+    assert pagination.page_size == const.PAGE_SIZE
+    assert pagination.total_pages == uploaded_maxed_metadata.total_pages
+
+# TODO: Test failing conditions
