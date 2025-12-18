b63773cbd1065106037f32b131e1e0bd1d705fcb
cusma <cosimo.bassi@algorand.foundation>
2025-12-18 11:44:08 -0300

    test: set immutable

---
+++ tests/test_arc89_set_immutable.py
@@ -0,0 +1,33 @@
+from algokit_utils import CommonAppCallParams, SigningAccount
+
+from smart_contracts.artifacts.asa_metadata_registry.asa_metadata_registry_client import (
+    Arc89SetImmutableArgs,
+    AsaMetadataRegistryClient,
+)
+from tests.helpers.factories import AssetMetadata
+
+
+def test_set_immutable_flag(
+    asset_manager: SigningAccount,
+    asa_metadata_registry_client: AsaMetadataRegistryClient,
+    uploaded_short_metadata: AssetMetadata,
+) -> None:
+    asset_id = uploaded_short_metadata.asset_id
+
+    # Verify initial state is False
+    assert not uploaded_short_metadata.is_immutable
+
+    # Set metadata as immutable and verify
+    asa_metadata_registry_client.send.arc89_set_immutable(
+        args=Arc89SetImmutableArgs(asset_id=asset_id),
+        params=CommonAppCallParams(sender=asset_manager.address),
+    )
+
+    post_set = AssetMetadata.from_box_value(
+        asset_id,
+        asa_metadata_registry_client.state.box.asset_metadata.get_value(asset_id),
+    )
+    assert post_set.is_immutable
+
+
+# TODO: Test failing conditions
