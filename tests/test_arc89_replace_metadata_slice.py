10fe32211f31a956ebede50002d02ee4ce8b7fa4
cusma <cosimo.bassi@algorand.foundation>
2025-12-20 15:33:09 -0300

    test: replace metadata slice

---
+++ tests/test_arc89_replace_metadata_slice.py
@@ -0,0 +1,53 @@
+import pytest
+from algokit_utils import CommonAppCallParams, SigningAccount
+
+from smart_contracts.artifacts.asa_metadata_registry.asa_metadata_registry_client import (
+    Arc89ReplaceMetadataSliceArgs,
+    AsaMetadataRegistryClient,
+)
+from tests.helpers.factories import AssetMetadata
+
+
+@pytest.mark.parametrize(
+    "metadata_fixture",
+    [
+        "mutable_short_metadata",
+        "mutable_json_obj_metadata",
+    ],
+)
+def test_replace_metadata_slice(
+    asset_manager: SigningAccount,
+    asa_metadata_registry_client: AsaMetadataRegistryClient,
+    metadata_fixture: str,
+    request: pytest.FixtureRequest,
+) -> None:
+    metadata: AssetMetadata = request.getfixturevalue(metadata_fixture)
+    asset_id = metadata.asset_id
+
+    offset = 0
+    size = 4
+    current_slice = metadata.metadata_bytes[offset : offset + size]
+    new_slice = size * b"\x00"
+    assert current_slice != new_slice
+
+    asa_metadata_registry_client.send.arc89_replace_metadata_slice(
+        args=Arc89ReplaceMetadataSliceArgs(
+            asset_id=asset_id,
+            offset=offset,
+            payload=new_slice,
+        ),
+        params=CommonAppCallParams(sender=asset_manager.address),
+    )
+    updated_metadata = AssetMetadata.from_box_value(
+        asset_id,
+        asa_metadata_registry_client.state.box.asset_metadata.get_value(asset_id),
+    )
+    replaced_slice = updated_metadata.metadata_bytes[offset : offset + size]
+    assert replaced_slice == new_slice
+    assert updated_metadata.size == metadata.size
+    assert updated_metadata.identifiers == metadata.identifiers
+    assert updated_metadata.flags == metadata.flags
+    assert updated_metadata.metadata_bytes != metadata.metadata_bytes
+
+
+# TODO: Test failing conditions
