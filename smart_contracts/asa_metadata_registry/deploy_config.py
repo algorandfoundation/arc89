0978567aab8966f7ef2e167d807bed9c66f1c0e4
cusma <cosimo.bassi@algorand.foundation>
2025-12-20 22:57:52 -0300

    chore: fund deployer from env

--- smart_contracts/asa_metadata_registry/deploy_config.py
+++ smart_contracts/asa_metadata_registry/deploy_config.py
@@ -2,19 +2,24 @@ import logging
 
 import algokit_utils
 
+from .constants import ACCOUNT_MBR
 from .template_vars import TRUSTED_DEPLOYER
 
 logger = logging.getLogger(__name__)
 
 
-# define deployment behaviour based on supplied app spec
 def deploy() -> None:
     from smart_contracts.artifacts.asa_metadata_registry.asa_metadata_registry_client import (
         AsaMetadataRegistryFactory,
     )
 
     algorand = algokit_utils.AlgorandClient.from_environment()
+    algorand.set_default_validity_window(100)
     deployer_ = algorand.account.from_environment("DEPLOYER")
+    algorand.account.ensure_funded_from_environment(
+        account_to_fund=deployer_,
+        min_spending_balance=algokit_utils.AlgoAmount(algo=1),
+    )
 
     factory = algorand.client.get_typed_app_factory(
         AsaMetadataRegistryFactory,
@@ -35,7 +40,7 @@ def deploy() -> None:
     ]:
         algorand.send.payment(
             algokit_utils.PaymentParams(
-                amount=algokit_utils.AlgoAmount(algo=1),
+                amount=algokit_utils.AlgoAmount(micro_algo=ACCOUNT_MBR),
                 sender=deployer_.address,
                 receiver=app_client.app_address,
             )
