750babb835bdcb567b87a9508b8d9d5cc5a10fa5
cusma <cosimo.bassi@algorand.foundation>
2025-12-22 22:47:06 -0300

    chore: update flags definitions

--- tests/test_arc89_set_irreversible_flag.py
+++ tests/test_arc89_set_irreversible_flag.py
@@ -13,17 +13,20 @@ from tests.helpers.utils import set_flag_and_verify
 
 
 @pytest.mark.parametrize(
-    "flag,check_fn",
+    "irreversible_flag,check_fn",
     [
-        (flags.FLG_RESERVED_6, lambda m: bool(m.flags & bitmasks.MASK_RESERVED_6)),
-        (flags.FLG_IMMUTABLE, lambda m: m.is_immutable),
+        (flags.IRR_FLG_RESERVED_2, lambda m: bool(m.irreversible_flags & bitmasks.MASK_IRR_RESERVED_2)),
+        (flags.IRR_FLG_RESERVED_3, lambda m: bool(m.irreversible_flags & bitmasks.MASK_IRR_RESERVED_3)),
+        (flags.IRR_FLG_RESERVED_4, lambda m: bool(m.irreversible_flags & bitmasks.MASK_IRR_RESERVED_4)),
+        (flags.IRR_FLG_RESERVED_5, lambda m: bool(m.irreversible_flags & bitmasks.MASK_IRR_RESERVED_5)),
+        (flags.IRR_FLG_RESERVED_6, lambda m: bool(m.irreversible_flags & bitmasks.MASK_IRR_RESERVED_6)),
     ],
 )
 def test_set_irreversible_flags(
     asset_manager: SigningAccount,
     asa_metadata_registry_client: AsaMetadataRegistryClient,
     mutable_short_metadata: AssetMetadata,
-    flag: int,
+    irreversible_flag: int,
     check_fn: Callable[[AssetMetadata], bool],
 ) -> None:
     # Verify initial state is False
@@ -34,7 +37,7 @@ def test_set_irreversible_flags(
         asa_metadata_registry_client,
         asset_manager,
         mutable_short_metadata,
-        flag,
+        irreversible_flag,
         check_fn,
         reversible=False,
     )
--- tests/test_arc89_set_reversible_flag.py
+++ tests/test_arc89_set_reversible_flag.py
@@ -13,19 +13,23 @@ from tests.helpers.utils import set_flag_and_verify
 
 
 @pytest.mark.parametrize(
-    "flag,check_fn",
+    "reversible_flag,check_fn",
     [
-        (flags.FLG_ARC20, lambda m: m.is_arc20),
-        (flags.FLG_ARC62, lambda m: m.is_arc62),
-        (flags.FLG_RESERVED_2, lambda m: bool(m.flags & bitmasks.MASK_RESERVED_2)),
-        (flags.FLG_RESERVED_3, lambda m: bool(m.flags & bitmasks.MASK_RESERVED_3)),
+        (flags.REV_FLG_ARC20, lambda m: m.is_arc20),
+        (flags.REV_FLG_ARC62, lambda m: m.is_arc62),
+        (flags.REV_FLG_RESERVED_2, lambda m: bool(m.reversible_flags & bitmasks.MASK_REV_RESERVED_2)),
+        (flags.REV_FLG_RESERVED_3, lambda m: bool(m.reversible_flags & bitmasks.MASK_REV_RESERVED_3)),
+        (flags.REV_FLG_RESERVED_4, lambda m: bool(m.reversible_flags & bitmasks.MASK_REV_RESERVED_4)),
+        (flags.REV_FLG_RESERVED_5, lambda m: bool(m.reversible_flags & bitmasks.MASK_REV_RESERVED_5)),
+        (flags.REV_FLG_RESERVED_6, lambda m: bool(m.reversible_flags & bitmasks.MASK_REV_RESERVED_6)),
+        (flags.REV_FLG_RESERVED_7, lambda m: bool(m.reversible_flags & bitmasks.MASK_REV_RESERVED_7)),
     ],
 )
 def test_set_and_clear_reversible_flags(
     asset_manager: SigningAccount,
     asa_metadata_registry_client: AsaMetadataRegistryClient,
     mutable_short_metadata: AssetMetadata,
-    flag: int,
+    reversible_flag: int,
     check_fn: Callable[[AssetMetadata], bool],
 ) -> None:
     # Verify initial state is False
@@ -36,7 +40,7 @@ def test_set_and_clear_reversible_flags(
         asa_metadata_registry_client,
         asset_manager,
         mutable_short_metadata,
-        flag,
+        reversible_flag,
         check_fn,
         value=True,
     )
@@ -46,7 +50,7 @@ def test_set_and_clear_reversible_flags(
         asa_metadata_registry_client,
         asset_manager,
         mutable_short_metadata,
-        flag,
+        reversible_flag,
         check_fn,
         value=False,
     )
