6ed554b92f5c6532692ebd42ef2b576bf858e860
cusma <cosimo.bassi@algorand.foundation>
2025-12-24 14:53:51 -0300

    test: arc compliance

--- tests/test_arc89_create_metadata.py
+++ tests/test_arc89_create_metadata.py
@@ -1,10 +1,11 @@
 import pytest
-from algokit_utils import SigningAccount
+from algokit_utils import AssetCreateParams, SigningAccount
 
 from smart_contracts.artifacts.asa_metadata_registry.asa_metadata_registry_client import (
     AsaMetadataRegistryClient,
 )
-from tests.helpers.factories import AssetMetadata
+from smart_contracts.asa_metadata_registry import constants as const
+from tests.helpers.factories import AssetMetadata, create_arc3_payload
 from tests.helpers.utils import create_metadata
 
 
@@ -14,7 +15,6 @@ from tests.helpers.utils import create_metadata
         "empty_metadata",
         "short_metadata",
         "maxed_metadata",
-        "json_obj_metadata",
     ],
 )
 def test_create_metadata(
@@ -34,6 +34,112 @@ def test_create_metadata(
     )
     assert mbr_delta.amount == creation_mbr_delta.amount.micro_algo
     # TODO: Verify Asset Metadata Box contents matches fixture data
+
+
+@pytest.mark.parametrize(
+    "asset_params,arc3_compliant,arc89_native",
+    [
+        pytest.param(
+            {"asset_name": const.ARC_3_NAME.decode()},
+            True,
+            False,
+            id="arc3_name",
+        ),
+        pytest.param(
+            {"asset_name": "Test" + const.ARC_3_NAME_SUFFIX.decode()},
+            True,
+            False,
+            id="arc3_name_suffix",
+        ),
+        pytest.param(
+            {"url": "URI" + const.ARC_3_URL_SUFFIX.decode()},
+            True,
+            False,
+            id="arc3_url",
+        ),
+    ],
+)
+def test_arc3_compliance(
+    asset_manager: SigningAccount,
+    asa_metadata_registry_client: AsaMetadataRegistryClient,
+    asset_params: dict,
+    *,
+    arc3_compliant: bool,
+    arc89_native: bool,
+) -> None:
+    asset_id = asa_metadata_registry_client.algorand.send.asset_create(
+        params=AssetCreateParams(
+            sender=asset_manager.address,
+            manager=asset_manager.address,
+            total=42,
+            **asset_params,
+        )
+    ).asset_id
+
+    metadata = AssetMetadata.create(
+        asset_id=asset_id,
+        metadata=create_arc3_payload(name="ARC3 Compliant Test"),
+        arc3_compliant=arc3_compliant,
+        arc89_native=arc89_native,
+    )
+
+    assert metadata.is_arc3 == arc3_compliant
+    if arc89_native:
+        assert metadata.is_arc89_native
+
+    create_metadata(
+        asset_manager=asset_manager,
+        asa_metadata_registry_client=asa_metadata_registry_client,
+        asset_id=asset_id,
+        metadata=metadata,
+    )
+
+    created_metadata = AssetMetadata.from_box_value(
+        asset_id,
+        asa_metadata_registry_client.state.box.asset_metadata.get_value(asset_id),
+    )
+    assert created_metadata.is_arc3 == arc3_compliant
+    if arc89_native:
+        assert created_metadata.is_arc89_native
+
+
+def test_arc89_native_arc3_url_compliance(
+    asset_manager: SigningAccount,
+    asa_metadata_registry_client: AsaMetadataRegistryClient,
+    arc90_uri: str,
+) -> None:
+    asset_id = asa_metadata_registry_client.algorand.send.asset_create(
+        params=AssetCreateParams(
+            sender=asset_manager.address,
+            manager=asset_manager.address,
+            total=42,
+            url=arc90_uri + const.ARC_3_URL_SUFFIX.decode(),
+        )
+    ).asset_id
+
+    metadata = AssetMetadata.create(
+        asset_id=asset_id,
+        metadata=create_arc3_payload(name="ARC3 Compliant Test"),
+        arc3_compliant=True,
+        arc89_native=True,
+    )
+
+    assert metadata.is_arc3
+    assert metadata.is_arc89_native
+
+    create_metadata(
+        asset_manager=asset_manager,
+        asa_metadata_registry_client=asa_metadata_registry_client,
+        asset_id=asset_id,
+        metadata=metadata,
+    )
+
+    created_metadata = AssetMetadata.from_box_value(
+        asset_id,
+        asa_metadata_registry_client.state.box.asset_metadata.get_value(asset_id),
+    )
+    assert created_metadata.is_arc3
+    assert created_metadata.is_arc89_native
 
 
 # TODO: Test failing conditions
