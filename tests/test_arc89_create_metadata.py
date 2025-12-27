8c068ff5732c3203363c1d79b21958afa42f6ca6
cusma <cosimo.bassi@algorand.foundation>
2025-12-27 18:05:06 -0300

    test: arc3 metadata hash

--- tests/test_arc89_create_metadata.py
+++ tests/test_arc89_create_metadata.py
@@ -5,7 +5,11 @@ from smart_contracts.artifacts.asa_metadata_registry.asa_metadata_registry_clien
     AsaMetadataRegistryClient,
 )
 from smart_contracts.asa_metadata_registry import constants as const
-from tests.helpers.factories import AssetMetadata, create_arc3_payload
+from tests.helpers.factories import (
+    AssetMetadata,
+    compute_arc3_metadata_hash,
+    create_arc3_payload,
+)
 from tests.helpers.utils import create_metadata
 
 
@@ -106,14 +110,14 @@ def test_arc3_compliance(
 def test_arc89_native_arc3_url_compliance(
     asset_manager: SigningAccount,
     asa_metadata_registry_client: AsaMetadataRegistryClient,
-    arc90_uri: str,
+    arc89_partial_uri: str,
 ) -> None:
     asset_id = asa_metadata_registry_client.algorand.send.asset_create(
         params=AssetCreateParams(
             sender=asset_manager.address,
             manager=asset_manager.address,
             total=42,
-            url=arc90_uri + const.ARC3_URL_SUFFIX.decode(),
+            url=arc89_partial_uri + const.ARC3_URL_SUFFIX.decode(),
         )
     ).asset_id
 
@@ -140,6 +144,49 @@ def test_arc89_native_arc3_url_compliance(
     )
     assert created_metadata.is_arc3
     assert created_metadata.is_arc89_native
+
+
+def test_arc3_metadata_hash(
+    asset_manager: SigningAccount,
+    asa_metadata_registry_client: AsaMetadataRegistryClient,
+) -> None:
+    import json
+
+    arc3_payload = create_arc3_payload(name="ARC3 Compliant Test")
+    arc3_metadata_hash = compute_arc3_metadata_hash(json.dumps(arc3_payload).encode())
+    asset_id = asa_metadata_registry_client.algorand.send.asset_create(
+        params=AssetCreateParams(
+            sender=asset_manager.address,
+            manager=asset_manager.address,
+            total=42,
+            asset_name=const.ARC3_NAME.decode(),
+            metadata_hash=arc3_metadata_hash,
+        )
+    ).asset_id
+
+    metadata = AssetMetadata.create(
+        asset_id=asset_id,
+        metadata=arc3_payload,
+        immutable=True,
+        arc3_compliant=True,
+        asset_metadata_hash=arc3_metadata_hash,
+    )
+    assert metadata.is_arc3
+    assert metadata.metadata_hash == arc3_metadata_hash
+
+    create_metadata(
+        asset_manager=asset_manager,
+        asa_metadata_registry_client=asa_metadata_registry_client,
+        asset_id=asset_id,
+        metadata=metadata,
+    )
+
+    created_metadata = AssetMetadata.from_box_value(
+        asset_id,
+        asa_metadata_registry_client.state.box.asset_metadata.get_value(asset_id),
+    )
+    assert created_metadata.is_arc3
+    assert metadata.metadata_hash == arc3_metadata_hash
 
 
 # TODO: Test failing conditions
